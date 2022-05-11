9338051c64c355cee3ac693de91631e8a0c110a6
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-11 09:42:28 +0200

    routing page with span, onclick, setcurrentpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-// import { useState } from "react";
+import { useState } from "react";
+import React from "react";
 import "./App.scss";
 import { PageWelcome } from "./pages/PageWelcome";
 import { PageBooks } from "./pages/PageBooks";
 import { PageAbout } from "./pages/PageAbout";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(`welcome`);
   return (
     <div className="App">
       <h1>Personal Site</h1>
       <hr />
-      Welcome | Books | About
+      <span onClick={() => setCurrentPage("welcome")}>Welcome</span> |
+      <span onClick={() => setCurrentPage("books")}>Books</span> |
+      <span onClick={() => setCurrentPage("about")}>About</span>
       <hr />
-      <PageWelcome />
-      <PageBooks />
-      <PageAbout />
+      {currentPage === "welcome" && <PageWelcome />}
+      {currentPage === "books" && <PageBooks />}
+      {currentPage === "about" && <PageAbout />}
     </div>
   );
 }
