9d4a722baeb24523174fa245db976b74a03ff89d
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-11 10:27:01 +0200

    gutenberg books with axios added to books page. bug to fix..

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const booksUrl = "https://gutendex.com/books/?search=berlin";
+
 export const PageBooks = () => {
+  const [bookData, setBookData] = useState({ count: 0 });
+
+  useEffect(() => {
+    (async () => {
+      setBookData((await axios.get(booksUrl)).data);
+    })();
+  });
   return (
     <>
       <h1>Books</h1>
-      <h2>There are no books yet</h2>
+      <h2>There are {bookData.count} books yet</h2>
+      {bookData.results.map((book, index) => {})}
     </>
   );
 };
